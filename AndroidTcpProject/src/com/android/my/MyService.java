1da19a43b32b8dd91fa927441167f5a9d78ed6eb
wmbilibili <2609228707@qq.com>
2023-02-24 17:48:19 +0800

    V1.0.7 优化服务端线程销毁逻辑 [author]        wm [description] 优化服务端线程销毁逻辑

--- AndroidTcpProject/src/com/android/my/MyService.java
+++ AndroidTcpProject/src/com/android/my/MyService.java
@@ -17,22 +17,25 @@ public class MyService {
     private static PrintWriter clientOut;
     private static BufferedReader clientIn;
 
+    public static ConcurrentHashMap<String, Thread> consoleThreadMap = new ConcurrentHashMap<String, Thread>();
+    private static Thread outToClientThread;
+    private static ThreadPoolExecutor tpe;
+
     /**
      * 创建线程池
      */
-    private static ExecutorService threadPool = new ThreadPoolExecutor(2,
-            5,
+    private static ExecutorService threadPool = new ThreadPoolExecutor(4,
+            8,
             2L,
             TimeUnit.SECONDS,
-            new ArrayBlockingQueue<Runnable>(3),
+            new ArrayBlockingQueue<Runnable>(4),
             Executors.defaultThreadFactory(),
             new ThreadPoolExecutor.DiscardOldestPolicy());
 
 
     public static void main(String[] args) {
-        System.out.println("hello world");
         scanner = new Scanner(System.in);
-
+        tpe = (ThreadPoolExecutor) threadPool;
         startService();
     }
 
@@ -46,13 +49,16 @@ public class MyService {
                 ServerSocket serverSocket = new ServerSocket(SERVERPORT);
                 //循环监听客户的连接
                 while (true) {
+                    System.out.println("startService--activityThread size :"+tpe.getActiveCount());
                     System.out.println("Server: waiting...");
                     //这里会阻塞，直到有客户的连接
                     client = serverSocket.accept();
+                    System.out.println(client);
                     System.out.println("Server: Receiving...");
                     try {
                         //开启输入线程，用于控制发送消息到客户端、控制关闭客户
                         sendMessageAndControlStop();
+                        System.out.println("connectClient--activityThread size :"+tpe.getActiveCount());
                         while (true) {
                             if (client.isClosed()) {
                                 //若客户连接已关闭，就退出循环，不再接收当前客户的消息
@@ -64,9 +70,7 @@ public class MyService {
                             String str = clientIn.readLine();
                             if (str == null || "".equals(str) || "null".equals(str)) {
                                 //读取到的消息为空，证明该客户已断开连接
-                                clientIn.close();
-                                clientOut.close();
-                                client.close();
+                                closeClient();
                                 break;
                             }
                             System.out.println("Server: Received: '" + str + "'");
@@ -75,9 +79,7 @@ public class MyService {
                         System.out.println("Client: Error");
                         e.printStackTrace();
                     } finally {
-                        clientIn.close();
-                        clientOut.close();
-                        client.close();
+                        closeClient();
                         System.out.println("Client: disconnect.");
                     }
                 }
@@ -94,7 +96,9 @@ public class MyService {
     public static void sendMessageAndControlStop() {
         threadPool.execute(() -> {
             try {
+                outToClientThread = Thread.currentThread();
                 clientOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
+                System.out.println("clientOut " + clientOut + "   outThread : " + outToClientThread);
                 while (true) {
                     //若客户端已关闭，则退出循环，关闭控制台输入监听
                     if (client.isClosed()) {
@@ -104,9 +108,7 @@ public class MyService {
                     String str = scanner.nextLine();
                     if ("stop".equals(str)) {
                         System.out.println("input stop");
-                        clientIn.close();
-                        clientOut.close();
-                        client.close();
+                        closeClient();
                         break;
                     } else {
                         clientOut.println(str);
@@ -118,6 +120,24 @@ public class MyService {
                 e.printStackTrace();
             }
         });
+    }
+
+
+    public static void closeClient(){
+        try {
+            clientIn.close();
+            clientOut.close();
+            client.close();
+            if (!outToClientThread.isInterrupted()){
+                //当前活动线程数
+                outToClientThread.interrupt();
+                System.out.println("outThread is interrupt :"+outToClientThread.isInterrupted());
+                System.out.println("closeClient activityThread size :"+tpe.getActiveCount());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
